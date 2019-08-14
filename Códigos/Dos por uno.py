0fe6722b6e043470faac5c4daa58e32972ac2a84
Gabriel Martínez <gmll920628@gmail.com>
2019-08-14 14:03:10 -0500

    Primeros cambios hechos por recomendación del editor.

--- Códigos/Dos por uno.py
+++
@@ -1,9 +0,0 @@
-def Dosxuno(*args):
-    if len(args) == 0:
-        print("One for you, one for me")
-    else:
-        print("one for {x}, one for you".format(x=args[0]))
-
-
-Dosxuno()
-Dosxuno("Galleta")
