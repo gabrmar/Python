327c569e5e90b826aefd5b248d962d626bad46c3
Gabriel Martínez <gmll920628@gmail.com>
2019-08-23 14:26:52 -0500

    Creación de condicionales para detectar años bisiestos.

--- Códigos/Año Bisiesto.py
+++ Códigos/Año Bisiesto.py
@@ -1,4 +1,12 @@
 def Esbisiesto(año):
-    if año % 4 == 0:
-        print("Divisible por 4")
-Esbisiesto(1000)
+        if año % 4 == 0:
+           if año % 100 == 0:
+                if año % 400 == 0:
+                        print("Es bisiesto")
+                else:
+                        print("No es bisiesto 1")
+        else:
+                print("No es bisiesto 2")
+        
+                
+Esbisiesto(2000)
