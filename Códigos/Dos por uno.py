a7efd5b2b967ea7f7b03d33e951dc21c96728cc2
Gabriel Martínez <gmll920628@gmail.com>
2019-09-02 15:54:40 -0500

    Correción de mensaje en Dos por uno.py hacer envié el mensaje por medio del return.

--- Códigos/Dos por uno.py
+++ Códigos/Dos por uno.py
@@ -1,10 +1,14 @@
 def Dosxuno(name="you"):
         if isinstance(name, str):
-            print("one for {}, one for you".format(name))
+            word = "one for " + str(name) +", one for me"
         else:
             raise TypeError("Invalid parameter supplied, this function accepts only string as a parameter.")
+        return word
 
-Dosxuno()
-Dosxuno("Galleta")
-Dosxuno(1)
-Dosxuno(True)
+hola = Dosxuno()
+print(hola)
+hola2 = Dosxuno("Galleta")
+print(hola2)
+#-----quita los comentarios de abajo para generar excepciones de tipo-----#
+#hola3 = Dosxuno(1)
+#hola4 = Dosxuno(True)
