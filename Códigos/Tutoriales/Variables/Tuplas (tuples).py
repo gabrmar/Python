ed9363492e170a828c49f887d23bd1aac68392e8
Gabriel Martínez <gmll920628@gmail.com>
2019-09-06 14:39:35 -0500

    Aportes de la clase sobre tuplas de Platzi.

--- Códigos/Tutoriales/Variables/Tuplas (tuples).py
+++ Códigos/Tutoriales/Variables/Tuplas (tuples).py
@@ -4,12 +4,21 @@ Similares a las listas, las tuplas son secuencias de valores que no puede ser mo
 """
 tupla = ("galleta","pollo",3.14632)
 tupla2 = "hola",3,2,-1 #se puede crear tuplas sin paréntesis
+tupla_platzi = (1,) #Esta es la manera en que se crean tuplas de un solo elemento (el elemento 1).
+# esto es necessario para que el elemento no corra el riesgo de ser reconocido como otro tipo de variable
+# en el ejemplo de tupla_platzi, si no ser hace (1,) y se opta por (1) o 1 simplemente, entonces Python
+# tomará esa variable como un entero y no como tupla.
 print(tupla)
 print(tupla2)
+print(tupla_platzi)
 #tupla[0] = 20 #quita el comentario de esta línea y tendrás un error de tipo porque no se pueden asginar elementos
 # a una tupla luego de su creación
 tupla3 = () #tupla vacía
 print(tupla3)
+
+#Las tuplas no se pueden modifcar como se hace en el caso de una lista, sino que es necesaria la creación
+# de una nueva tupla que tenga los cambios que se requieren. En eso se puede decir que hay simulitud entre 
+# una tupla y una variable string.
 
 #Desempacando tuplas
 print ("-------------------")
