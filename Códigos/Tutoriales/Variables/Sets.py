44a8e4481047f656e8094b3d738e4ff4eeb216d2
Gabriel Martínez <gmll920628@gmail.com>
2019-09-09 14:00:13 -0500

    Correción de comentarios y adición de alternativas para hacer operaciones con sets en Python.

--- Códigos/Tutoriales/Variables/Sets.py
+++ Códigos/Tutoriales/Variables/Sets.py
@@ -1,7 +1,8 @@
 """
-Los set son estructuras de datos similares a los direccionarios, de hecho
+Los set son estructuras de datos similares a los diccionarios, de hecho
 también son formados por medio de llaves {}, pero también pueden ser formados
 por el uso de listas (agrupadas en corchetes) y la función set(lista)
+Los sets pueden ser vistos como conjuntos matemáticos.
 """
 lista = ["hola",3,"PI"]
 set1 = set(lista)
@@ -20,10 +21,10 @@ if 4 in set2:
 #Características de los Sets
 #1: no son indexados
 #2: No eliminan de manera automática elemetos repetidos
-#3: son variables  de las cuales se pueden obtener datos de forma más rápida
-# que las estructuras de datos indexados
-nums = {1, 2, 1, 3, 1, 4, 5, 6}
-print(nums)
+#3: Son variables  de las cuales se pueden obtener datos de forma más rápida
+#4: No se trabaja con datos repetidos. Puedes agregarlos pero no son tenidos en cuenta
+nums = {1, 2, 1, 3, 1, 4, 5, 6, 8, 8}
+print("Los elementos repetidos no se muestra en los sets: {}".format(nums))
 nums.add(-7) # añade el -7 a la lista
 nums.remove(3) # remueve el 3 de la lista
 print(nums)
@@ -38,9 +39,11 @@ segundo = {4, 5, 6, 7, 8, 9}
 print(primer)
 print(segundo)
 print(primer | segundo) # operación de unión (eliminando repetidos)
+print(primer.union(segundo) # Forma alternativa de hacer la unión
 print(primer & segundo) # elementos en común (interesección)
 print(primer - segundo) # muuestra los elementos que están en el primer set
 # que no hacen parte del segundo (diferencia)
+print(primer.difference(segundo)) # Forma alternativa de operación diferencia.
 print(segundo - primer) # muestra los elementos que están en el segundo set
 # que no hacen parte del primero
 print(primer ^ segundo) #Muestra los elementos únicos en cada set en un sólio
