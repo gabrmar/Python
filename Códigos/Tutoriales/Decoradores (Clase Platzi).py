18bcf1bf946658dad9c7b495d2416013e58280da
Gabriel Martínez <gmll920628@gmail.com>
2019-09-16 16:53:33 -0500

    Comentarios al código de decoradores de Python. Necesito buscar más información al respecto.

--- Códigos/Tutoriales/Decoradores (Clase Platzi).py
+++ Códigos/Tutoriales/Decoradores (Clase Platzi).py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
-
-
+# Definición de función de protección de contraseña
 def protected(func):
-
+    
+    # Definición de función que mensaje de respuesta si la contraseña es acertada o no
     def wrapper(password):
 
         if password == 'platzi':
+            # Regresar una función si se cumple la contraseña platzi
             return func()
         else:
             print('La contraseña es incorrecta')
-
+    # Regresar objeto wrapper. Nota: no regresar la función wrapper()
+    # porque genera un error. 
     return wrapper
 
 
@@ -18,7 +19,6 @@ def protected_func():
     print('Tu contraseña es correcta.')
 
 
-if __name__ == '__main__':
-    password = input('Ingresa tu contraseña: ')
-
-    protected_func(password)
+if __name__ == '__main__': #Función principal
+    password = input('Ingresa tu contraseña: ') #Ingreso de contraseña
+    protected_func(password) # Función de contraseña con decorador
