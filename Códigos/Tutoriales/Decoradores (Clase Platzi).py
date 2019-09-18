974c88cf87c67aeda9e966177350e58cbb125454
Gabriel Martínez <gmll920628@gmail.com>
2019-09-18 14:41:05 -0500

    comentarios adicionales al código de decoradores.

--- Códigos/Tutoriales/Decoradores (Clase Platzi).py
+++ Códigos/Tutoriales/Decoradores (Clase Platzi).py
@@ -1,16 +1,27 @@
+
+#-----Clase de Python en Platzi sobre decoradores-------#
+
 # Definición de función de protección de contraseña
+# Cuando se trabaja con decoradores, lo usual es trabajar con fucniones anidadas.
+# protected será la función principal de un nido de funciones la cual se le conoce
+# como función de cierre ya que define (cierra, delimita)  el inicio y cierre del
+# nido de funciones.
 def protected(func):
     
     # Definición de función que mensaje de respuesta si la contraseña es acertada o no
+    # Ésta función se le conoce como wrapper cuando se habla de decoradores.
+    # Un wrapper es la función que se encuentra anidada dentro de la función principal
+    # func es la función de entrada de la función protected.
     def wrapper(password):
-
+        
         if password == 'platzi':
             # Regresar una función si se cumple la contraseña platzi
-            return func()
+            return func() #regresar la funcion con paréntesis hace que la función se
+            # ejecute cuando sea regresada.
         else:
             print('La contraseña es incorrecta')
     # Regresar objeto wrapper. Nota: no regresar la función wrapper()
-    # porque genera un error. 
+    # porque genera un error al indicar que le falta un argumento a la función wrapper.
     return wrapper
 
 
