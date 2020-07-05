d4dddfc319b3f9be20ea104afec4f743061ca091
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-07-05 14:19:26 -0500

    Finalización de comentarios en código de ciclo for.

--- Códigos/Tutoriales/1. Fundamentos/1.5 Ciclo for.py
+++ Códigos/Tutoriales/1. Fundamentos/1.5 Ciclo for.py
@@ -5,7 +5,8 @@ que ciertos comandos se ejecuten cierto número de veces. A este tipo de ejecuci
 los ciclos más comunes es el ciclo for (para). El suguiente código es un ejemplo de como implementar un ciclo for. El código utiliza los
  siguientes tipos de variables como formas de crear un ciclo for en Python:
 
--Listas: Una agrupación de variables de uno o más tipos dentro de un solo nombre que representa a todo el grupo.
+-Listas: Una agrupación de variables de uno o más tipos dentro de un solo nombre que representa a todo el grupo. Más información de las listas
+Al final del código.
 -Cadenas de caracteres (Strings): Es la forma en que las palabras son almacenadas en cualquier lenguaje de programación. Consta de una 
 agrupación de cracteres. Una cadena de cracteres puede verse como si fuera una lista conformada únicamente por caracteres.
 
@@ -16,25 +17,24 @@ palabra = "Programación" # variable string que contiene la palabra programacón
 for letra in palabras: # La estructura de un ciclo for es la siguiente: for contador in lista o string:
     """La variable letra revisa letra por letra el contenido de la variable palabra. Es decir que primero busca la primera letra (p), luego
     la segunda (r) y así sucesivamente hasta llegar a la última (n). """
-    print(letra) # Acá se muestra la letra guardaa 
-#el ciclo for puede usar el contador word para ir imprimiendo
-# los valores presnetes en la lista palabras. el Uso del operador
-# de lista in nos permite que el contador word sólo se incremente
-# dentro de los valores que hacen parte de los índices de la lista.
-for i in range(10):
-    print("iteración número " + str(i))
-# los rangos puenden usarse  como límites del incremento de un contador
-# cuando se se defino un valor inicial para dicho contador, este comienza
-# en cero.
+    print(letra) # Acá se muestra la letra guardada 
+
+for i in range(10): # La función range nos permite crear una lista formada por números enteros. al usar 10 como valor, estamos formando una
+    # lista de números enteros que va desde cero hasta 9.
+    print("iteración número " + str(i)) # para mostrar una variable, puedes agregarla como una suma y coloarla dentro del str(variable) para
+    # que sea tratada como texto, no como número.
 
 for x in palabra:
  print(x)
-"""
-las variables tipo string pueden ser usadasa como límite del contador. De esta
-manera las strings hacen parte de los elementos iterables en python, es decir
-que son  aquellas variables u organizaciones de las mismas  sirven para regular
-el flujo del contador en un ciclo for
-"""
 
+# En Python las variables que sirven sirven para regular el flujo del contador en un ciclo for se le conocen como iterables.
+
+
+"""
+Referencias
+
+Listas en Python: https://devcode.la/tutoriales/listas-python/
+
+"""
  
 
