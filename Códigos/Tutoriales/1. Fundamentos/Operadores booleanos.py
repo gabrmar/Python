d42941525f50c77456a2b407bf23339884d63165
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-07-13 10:52:01 -0500

    Incio de comentarios introductorios en código de operadores booleanos.

--- Códigos/Tutoriales/1. Fundamentos/Operadores booleanos.py
+++ Códigos/Tutoriales/1. Fundamentos/Operadores booleanos.py
@@ -1,5 +1,15 @@
-#operadores booleanos
+"""
+operadores booleanos
 
+En python se pueden manejar muchos tipos de datos, desde enteros, fraciones, letras, palabras y mensajes completos. Adicionalmente, Python 
+también maneja un tipo de dato conocido como booleano. Sólo existen  dos tipos de valires booleanos: verdaero (True) y falso (False). La
+fucnión de los booleanos es ser usados para evaluar si alguna instrucción en Python es verdadera o falsa. Esto tiene aplicaciones más claras
+en códigos que veremos más adelante. Por ahora, es importante saber que los booleanos existen en Python y cómo se escriben en este lenguaje. 
+
+Éste código muestra cómo se escriben los booleanos en Python y algunas de sus operaciones básicas.
+
+"""
+ 
 variable = True #booleans start with capital letters
 variable_2 = False
 print(str(variable) + " and " + str(variable_2))
