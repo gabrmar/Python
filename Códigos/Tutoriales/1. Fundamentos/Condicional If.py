b226f7fbe9d46642393c0267137b224133a5ef2e
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-07-20 20:09:36 -0500

    Finalización de intrudcción de codigo incial sobre condicional if.

--- Códigos/Tutoriales/1. Fundamentos/Condicional If.py
+++ Códigos/Tutoriales/1. Fundamentos/Condicional If.py
@@ -6,6 +6,13 @@ cuáles instrucciones y cuales no dependiendo de una o varias condiciones. El si
 puedes dialogar dependiendo de lo que respondas. En este código no sólo se muestra cómo usar el condicional if sino como  pedir datos y 
 guardarlos en variables. 
 
--Ejemplo básico con comentarios
+La estructura de un condicional If en Python es la siguiente:
 
+if condición:
+    instrucciones en caso de ser verdad
+else:
+    instrucciones en caso de ser falsa
+
+La primera parte expresa qué condición queremos evaluar con el if y las instrucciones a ejecutar si al condición es cierta. En caso de que sea
+falsa, entonces se ejecutan las instruccciones de la sección else.
 """
