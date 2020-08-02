57956812414a31c1eb6e4bda7fb815801796e185
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-08-02 12:55:50 -0500

    Addición de ejemplos de variables privadas, atributos por defecto y el uso del None en Python.

--- Códigos/Tutoriales/Programación Orientada a Objetos/Objetos.py
+++ Códigos/Tutoriales/Programación Orientada a Objetos/Objetos.py
@@ -4,14 +4,21 @@
 class gato: #creando el objeto/clase gato
     especie = "gato quillero" #este espacio es para los atributos de clase
     #se pueden ver como variables globales dentro de la clase
-    def __init__(self,color,piernas,nombre): #la función/método __init__ es  
+    def __init__(self,color,piernas,nombre,dieta = "Atún"): #la función/método __init__ es  
         # el connstructor de clase. Es el encargado de definir
         # los atributos de la clase (las variables que estarán
         # (asociadas a este objeto).
         # es necesario colocar como primer argumento la declaración self
+        # para el caso de la dieta, se está definiendo un valor por defecto en caso de que el usuario no lo especifique.
         self.color = color #self asocia las variables a la clase
         self.piernas = piernas
         self.nombre = nombre
+        self.dieta = dieta
+        self._raza = "Gato persa" # Indicando por convención por medio del guión bajo que la raza es una variable privada la cual el usuario
+        # no conoce ni va a interactuar con ella.
+        self._edad = None # El None es el indicador de que la varialble no tiene un valor definido. es como el Null en Java. En este caso
+        # se está efiniendo una variable privdad que se usará para el manejo de la edad del gato.
+
 
     def maullar(self):
         print("{x} usó maullar: miau!".format(x= self.nombre))
@@ -46,7 +53,7 @@ class métodos: #Se pueden crear clases sin atributos. Sólo con métodos
 
 felix = gato("rojo",4, "Felix")
 felix_jr = gatico("rojo",4,"felix JR.")
-print(felix.color)
+print(felix.color)  
 print(felix.piernas)
 felix.maullar() 
 print("La especie de {g} es {e}".format(g = felix.nombre,e = felix.especie))
