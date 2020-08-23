d645f63f199635b73b4fcb56e729393ac6fa4031
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-08-23 15:35:13 -0500

    Inclusión de párrafo explicativo sobre la privacidad en Python

--- Códigos/Tutoriales/Programación Orientada a Objetos/Objetos 3 (propiedades).py
+++ Códigos/Tutoriales/Programación Orientada a Objetos/Objetos 3 (propiedades).py
@@ -1,10 +1,21 @@
 # Propiedades en objetos de Python - Encapsulamiento
 
+"""
+Privacidad en Python
+
+La mayoría de lenguajes de programación soportan variables públicas y privadas para definit que tipos de datos en las instancias de clases
+pueden ser accedidos y modificados por los usuarios, sin embargo Python no es así, es decir que la privacidad tal cual como se conoce en
+otro lenguajes no existe en Python, pero existen ciertas restricciones que se le pueden colocar a las variables para tener cierto nivel
+de privacidad. Uno de los aspectos a tener en cuenta es la notación aceptada en Python para hacer saber a los programadores qué tipos de datos
+deberían ser tratados con privacidad el cual consiste en el uso de guiones bajos sencillos y dobles. Esto combinado con técnicas de programación
+para dar control de acceso se establece el nivel de privacidad que se necesite dependiendo de los requerimientos que el código debe cumplir. 
+"""
+
 class Pizza:
   def __init__(self, toppings):
     self.toppings = toppings
     self._pineapple_allowed = False # un guión bajo da un nivel de privacidad.
-    # este tipo de valores aparentemente no son importados.
+    # este tipo de valores aparentemente no son importados. 
 
 
 # Se pueden decorar métodos como propiedades para poder definir el tipo de acceso a los atributos. 
