b05d432c9a91f273a37d9ab88d198261f6547aff
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-08-29 21:44:45 -0500

    Creación de códigos de factorial de forma recursiva e iterativa.

--- Códigos/Complejidad algorítmica/Complejidad temporal.py
+++ Códigos/Complejidad algorítmica/Complejidad temporal.py
@@ -1,7 +1,24 @@
-# Teoría
+import time 
 
-# Código de factorial de forma interativa
+# Teorías
 
-# Código de factorial de forma recursiva
+def factorial_iterativo(x):
+    if x == 1:
+        return 1
+    else:    
+        fact = x
+        while x > 1:
+            fact = fact * x - 1
+            x = x - 1
+        return fact 
+
+def factorial_recursivo(x):
+  if x == 1: #Caso base de la recursividad.  Toda funnción
+      #recursiva debe tener un caso base que de final a la función,
+      # de lo contrario esta correrá hasta que aparezca un error de
+      # tiempo de ejecución (Runtime error)
+    return 1
+  else: 
+    return x * factorial(x-1)
 
 # Prueba de complejidad temporal
