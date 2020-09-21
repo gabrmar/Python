5ab3edd826acaacd1c4f2a2a1b4e7a5bc6798333
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-09-20 21:40:17 -0500

    Finalización de comentarios.

--- Códigos/Algoritmos de ordenamiento/Ordenamiento por mezcla.py
+++ Códigos/Algoritmos de ordenamiento/Ordenamiento por mezcla.py
@@ -19,41 +19,43 @@ def ordenamiento_por_mezcla(lista): # Definiendo función de ordenamiento por me
         k = 0
 
         while i < len(izquierda) and j < len(derecha): # Mientras los iteradores sean menores a las longitudes de las sub-listas
-            if izquierda[i] < derecha[j]:
-                lista[k] = izquierda[i]
-                i += 1
+            if izquierda[i] < derecha[j]: 
+                lista[k] = izquierda[i] # si es elemento de la sub-lista izquierda es menor, entonces se asigna su valor en la lista principal
+                i += 1 # i = i + 1
             else:
-                lista[k] = derecha[j]
-                j += 1
+                lista[k] = derecha[j] # En su defecto, se colocará el elemento de la sub-lista derecha
+                j += 1 # j = j + 1
 
-            k += 1
+            k += 1 # = k = k + 1
 
-        while i < len(izquierda):
-            lista[k] = izquierda[i]
+        while i < len(izquierda): # Mientras que el iterador i sea menor que la longitud de la sub-lista izquierda
+            lista[k] = izquierda[i]  # Colocando los valores de la sub-lista en la lista principal
             i += 1
             k +=1
 
-        while j < len(derecha):
-            lista[k] = derecha[j]
+        while j < len(derecha): # Mientras que el iterador j sea menor que la longitud de la sub-lista derecha
+            lista[k] = derecha[j] # Colocando los valores de la sub-lista en la lista principal
             j += 1
             k += 1
         
-        print(f'izquierda {izquierda}, derecha {derecha}')
-        print(lista)
-        print('-' * 50)
+        print(f'izquierda {izquierda}, derecha {derecha}') # Imprimiendo sub-listas en cada llamado de la función
+        print(lista) # Impresión de la lista ordenándose en cada una de las iteraciones. La lista se verá ordernada en esta línea en la 
+        # última iteración de esta función de ordenamiento por mezcla
+        print('-' * 50) # Imprimiendo una división formada por 50 guiones (-)
 
-    return lista
+    return lista # Regresando lista  
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Dunder init
     tamano_de_lista = int(input('De que tamano sera la lista? '))
 
     lista = [random.randint(0, 100) for i in range(tamano_de_lista)]
     print(lista)
-    print('-' * 20)
+    print('-' * 20) # Imprimiendo una división formada por 20 guiones (-)
+
 
     lista_ordenada = ordenamiento_por_mezcla(lista)
-    print(lista_ordenada)
+    print(lista_ordenada) # Imprimiendo lista ordenada 
 
 
 """
