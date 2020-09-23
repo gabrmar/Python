b840c37d1b1f933f2223f2522c2b27f2eed999c0
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-09-22 19:47:05 -0500

    Inicio de estructura para los códigos de funciones

--- Códigos/Tutoriales/1. Fundamentos/Funciones - Parte 1.py
+++ Códigos/Tutoriales/1. Fundamentos/Funciones - Parte 1.py
@@ -1,2 +1,7 @@
 # Colocar un par de ejemplos de funciones básicas
 
+"""
+1) Estructura básica de una función
+2) Funciones con parámetros 
+
+"""
--- Códigos/Tutoriales/1. Fundamentos/Funciones - Parte 2.py
+++ Códigos/Tutoriales/1. Fundamentos/Funciones - Parte 2.py
@@ -1,1 +1,6 @@
 # Colocar aspectos básicos de las funciones en Python que sean más propios del lenguaje
+
+"""
+3) Funciones como argumentos de funciones
+4) Renombre de funciones 
+"""
