edf2cc1dee991eba6ba746c88a856debeb5324ec
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-09-23 21:07:00 -0500

    Códigos báse sobre funciones.

--- Códigos/Tutoriales/1. Fundamentos/Funciones - Parte 1.py
+++ Códigos/Tutoriales/1. Fundamentos/Funciones - Parte 1.py
@@ -5,3 +5,17 @@
 2) Funciones con parámetros 
 
 """
+
+def suma(n1,n2):
+    suma = n1 + n2
+    return suma
+
+def saludo(nombre):
+    print(f"bienvenido al código de funciones, {nombre}")
+
+
+x = suma(1,2)
+print(x)
+print(suma(3,5))
+saludo("Max")
+saludo("María")
