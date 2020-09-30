a14a0729c58b30f7560a1edce23024071039c4e4
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-09-29 20:26:36 -0500

    Creación de base para trabajar en los códigos de strings en Python.

---
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/Strings.py
@@ -0,0 +1,11 @@
+
+"""
+- Introducción a las strings de Python
+- Formateo con valores numéricos  
+- Formateo con format()
+- Formateo con f""
+- Funciones báscias en strings en Python (Esto puede ir en otra función)
+- Compraciones con un IF en una string 
+- Slicing (Una tercera parte)
+- Iterables de strings en Python (Cuarta parte)
+"""
