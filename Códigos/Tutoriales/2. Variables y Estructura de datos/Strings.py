fa1b4ffaf3ab41c145e29312a1930287987b770a
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-10-03 16:58:11 -0500

    Adición de códigos de formateo de cadenas de caractéres.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/Strings.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/Strings.py
@@ -9,3 +9,25 @@
 - Slicing (Una tercera parte)
 - Iterables de strings en Python (Cuarta parte)
 """
+
+# Formateando variables
+x = input("Primer número:")
+x = int(x)
+x = x*2
+print("El valor número multiplicado por 2  es " + str(x))
+
+# Usando la función format
+y = input("Segundo número:")
+y = float(y)
+z = y*3.14
+print("{a} multilicado por 3.14 es {b}".format(a=y,b=z))
+
+# Usando f""
+print(f"Sumando los valores de {x} y {z}")
+suma = x + z
+print(f"Suma total:{suma}")
+
+
+"""
+1) Enlace de formateo/casteo de variables  en Python
+"""
