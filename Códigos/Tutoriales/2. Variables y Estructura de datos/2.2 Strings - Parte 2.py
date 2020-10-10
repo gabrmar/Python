ebbd69ec8370612dede9916b6191869248bf4479
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-10-10 14:17:48 -0500

    Correciones en comentarios de segundo código de strings.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.2 Strings - Parte 2.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.2 Strings - Parte 2.py
@@ -7,13 +7,13 @@
 # Basarse en ejemplos del código base de string
 
 
-lista = ["ella", "tú", "nosotros"]
+lista = ["ella", "tú", "nosotros"] # Definiendo lista de tres cadenas de caracteres (3 strings).
 l = len(lista) # Ésta función almacena el largo de la lista (cantidad de elementos) en una variable.
 print(l)
-string = ",".join(lista)  # transforma listas de strings en una sola por medio de un seperador (la coma esta vez)
+string = "-".join(lista)  # Transforma listas de strings en una sola string por medio de un seperador (el guión esta vez)
 # se puede usar cualquer caracter como separador entre los elementos de la lista
 print(string)
-string2 = "00000esta es la historia de Ramiro00000"
+string2 = "00000esta es la historia de Ramiro00000" # Ejemplo de una string con elementos en el extremo que no son de interés
 print(string2)
 string2 = string2.strip("0") # strip permite elminar caracteres que estén en los extremos de una string. En este caso el 0.
 # También sirve para elminar espacios de más en un string.
@@ -21,20 +21,20 @@ print(string2)
 string2 = string2.replace("Ramiro","Jaime") #reemplaza una parte de la string por otra
 print(string2)
 if string2.startswith("esta") == True: #pregunta si el string empieza con una palabra en especifico
-    # la parte de == True es más por  pedagogía, mas no es necesaria
+    # la parte de == True: es más por pedagogía, mas no es necesaria
     string2 = string2.replace("esta","Esta")
     print(string2)
-string3 = "bievenido a la ciudad de Cartagena"
+string3 = "bienvenido a la ciudad de Cartagena"
 if string3.endswith("Cartagena") == True: # pregunta si el string termina con uan palabra en especifico
-    # == True también es opcional
+    # == True: también es opcional
     string3 = string3.replace("Cartagena","Barranquilla")
     print(string3)
 string4 = "GaLleTa CON queso"
-string4 = string4.lower() #convertir string a minúscula
+string4 = string4.lower() # Convertir string a minúscula
 print(string4)
 string4 = string4.capitalize() # Convertir la primera letra del string en mayúscula
 print(string4)
 string4 = string4.upper() #convertir string a mayúscula
 print(string4)
-string = string.split(",")# opuesto a join, separa una string en elementos de una lista por medio de un separador
+string = string.split("-")# opuesto a join, separa una string en elementos de una lista por medio de un separador
 print(string)
