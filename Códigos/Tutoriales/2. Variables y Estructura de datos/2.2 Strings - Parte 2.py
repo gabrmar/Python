ed9240c68ace42a88df0685ca2994aa7485e55d8
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-10-11 10:34:29 -0500

    Inclusión de parte de condicionales  con strings

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.2 Strings - Parte 2.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.2 Strings - Parte 2.py
@@ -38,3 +38,29 @@ string4 = string4.upper() #convertir string a mayúscula
 print(string4)
 string = string.split("-")# opuesto a join, separa una string en elementos de una lista por medio de un separador
 print(string)
+
+#----------------------------------------------If con Strings--------------------------
+
+s1 = "hola"
+s2 = "Hola"
+if s1 > s2:
+    print("Hola")
+else:
+    print("Hola 2")
+# La compración entre cadenas de caracteres (strings) se hace la siguiente manera:
+"""
+Se hace comparación el primer caracter de cada  string. Si son iguales en valor ASCII,
+entonces se repite el proceso con los cracteres inmediatos. Si todos los caracteres
+correspondientes tienen el mismo valor ASCII, es porque las strings son iguales.
+
+NOTA: es importante resaltar que una letra en miníscula tiene un valor ASCII difernete
+al de su versión en mayúscula.
+"""
+
+"""
+Refrencias:
+
+1) Información sobre ASCII
+
+"""
+
