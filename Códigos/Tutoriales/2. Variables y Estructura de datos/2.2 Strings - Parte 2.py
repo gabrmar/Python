d3d2ae41f24c802e1a75ec88f480ebbbf807fc52
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-10-12 21:02:14 -0500

    Creación de tercera parte de strings para trabajar la parte de la sección de código ASCII aparte.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.2 Strings - Parte 2.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.2 Strings - Parte 2.py
@@ -1,9 +1,7 @@
 """
 - Funciones báscias en strings en Python (Esto puede ir en otra función)
 - Compraciones con un IF en una string así como el operador in y not in  
-- Función ord y Función chr (Quizás en un nuevo código) así como función max y min 
- Link: https://thepythonguru.com/python-strings/
- Buscar un enlace en español 
+
 """
 
 
@@ -42,28 +40,4 @@ print(string4)
 string = string.split("-")# opuesto a join, separa una string en elementos de una lista por medio de un separador
 print(string)
 
-#----------------------------------------------If con Strings--------------------------
-
-s1 = "hola"
-s2 = "Hola"
-if s1 > s2:
-    print("Hola")
-else:
-    print("Hola 2")
-# La compración entre cadenas de caracteres (strings) se hace la siguiente manera:
-"""
-Se hace comparación el primer caracter de cada  string. Si son iguales en valor ASCII,
-entonces se repite el proceso con los cracteres inmediatos. Si todos los caracteres
-correspondientes tienen el mismo valor ASCII, es porque las strings son iguales.
-
-NOTA: es importante resaltar que una letra en miníscula tiene un valor ASCII difernete
-al de su versión en mayúscula.
-"""
-
-"""
-Refrencias:
-
-1) Información sobre ASCII
-
-"""
 
---
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.3 Strings - Parte 3.py
@@ -0,0 +1,30 @@
+"""
+- Función ord y Función chr (Quizás en un nuevo código) así como función max y min 
+ Link: https://thepythonguru.com/python-strings/
+ Buscar un enlace en español 
+ """
+
+ #----------------------------------------------If con Strings--------------------------
+
+s1 = "hola"
+s2 = "Hola"
+if s1 > s2:
+    print("Hola")
+else:
+    print("Hola 2")
+# La compración entre cadenas de caracteres (strings) se hace la siguiente manera:
+"""
+Se hace comparación el primer caracter de cada  string. Si son iguales en valor ASCII,
+entonces se repite el proceso con los cracteres inmediatos. Si todos los caracteres
+correspondientes tienen el mismo valor ASCII, es porque las strings son iguales.
+
+NOTA: es importante resaltar que una letra en miníscula tiene un valor ASCII difernete
+al de su versión en mayúscula.
+"""
+
+"""
+Refrencias:
+
+1) Información sobre ASCII
+
+"""
