b2639135e38972f006bebca1a842322c78bc3376
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-10-19 18:55:38 -0500

    Creación de párrafo de introducción sobre cómmo sacar caracteres de una string.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.3 Strings - Parte 3.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.3 Strings - Parte 3.py
@@ -3,12 +3,24 @@
 - Explicar un poquito sobre strings y chars.
 - Posible introducción corta sobre ASCII y Unicode 
 
+Como se ha dicho antes, las variables strings son cadenas de símbolos llamados caracteres. Es posible acceder a los caracteres que
+hacen parte de una string por medio de un número que representa la posición del caracter que queremos extraer dentro de unos corchetes.
+Las posiciones de cada caracter  se cuentan desde el cero hasta el último valor, es decir que en el caso del string "hola", el conteo
+empieza en 0 que se reifere a la letra h y termina en 3 con la letra a.
+
  """
 
 x = "Mensjae"  # Variable string.
 a = x[1] # Sacando la letra que se encuentra en la posición 1 del string. Reucerda que un string es lo mismo que una lista de caracteres.
 # Gracias a ello es que se pueden aplicar muchas de las funciones de listas en una variable tipo string.
 print(a) # Motrando el caracter extraído del string
+
+
+"""
+ASCIII/UNICODE Aquí
+"""
+
+
 b = ord(a) # Obtiene el códgo ASCII/Unicode que representa el caracter
 print(f"El número que representa el caracter {a} es {b}.")
 code = 100 # Número entero cualquiera
