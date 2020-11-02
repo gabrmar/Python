d4a344dba5920ab55270f467e56c5f0dd2688a8c
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-11-02 10:24:53 -0500

    Finalzación de cuarta parte de strings en Python.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.4 String - Parte 4.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.4 String - Parte 4.py
@@ -1,15 +1,16 @@
-# licing y operaciones sobre una string - suma y multiplicación (Una tercera parte)
+"""
+Python te permite obtener partes de una cadena de caracteres; puede ser tanto un sólo elemento como una parte más grande de la cadena. Es
+decir que de la string "cadena" podríamos obtener cualquiera de sus letras así como sus sílabas, la mitad de la palabra o más. la parte
+que necesites la puedes obtener por medio de obtener partes de la string que en inglés se conocen como rebanadas (slices).
+"""
 
-x = "Galaeta"
-#Se pueden crear substrings por medio de lo que se llama en inglés como string slicing (slices)
-#Es la manera en que se pueden extraer pedazos de un cadena de caracteres en Python.
-print(x[0]) #Puedes pedir un caracter por medio de un índice (empiezan desde cero).
-print (x[0:2]) #pidiendo espacio desde la posición 0 hasta la 2. el elemento 0
-#Si aparece en la substring, pero el elemento 2 no. es decir que en [0:2] el 
-#segundo límite es no inclusivo, por lo cual la substring resultante constará
-#De los elementos desde la posición 0 hasta la 1.
-print(x[1:]) #Pidiendo los elementos desde la posición 1 hasta la última.
-print(x[0:4:2]) #Pidiendo una substring desde la posición 0 hasta la 3 (4 no includo)
-#En saltos de dos. 
-print(x[::-1])#Pidiendo string desde el principio hasta el final con pasos negativos
-# de -1. Esto isgnifica que se entrega la cadena de caracteres invertida
+x = "Galleta" 
+print(x[0]) # Puedes pedir un caracter por medio de un número que represente su posición dentro del string. El número 0 representa el
+# primer símbolo, el 1 el segundo y así sucesivamente
+print (x[0:2]) # Pidiendo los elementos que están desde la posición 0 hasta la 2. Se puede ver que el elemento 0 Sí es tomado pero el 
+# elemento 2 no, es decir que en [0:2] el segundo límite es no es incluído, por lo cual la porción tomada consta de los elementos desde la
+# posición 0 hasta la 1.
+print(x[1:]) # Pidiendo los elementos desde la posición 1 hasta la última
+print(x[0:4:2]) # Pidiendo una porción de la string desde la posición 0 hasta la 3 (4 no includo) saltando valores de dos en dos. 
+print(x[::-1])# Pidiendo una porción desde el principio hasta el final con pasos negativos de -1. Esto isgnifica que se entrega la cadena
+# de caracteres al revés
