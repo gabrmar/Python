9b6d2a6017864b21f85e55c084d57ca55b748b2d
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-11-02 10:45:25 -0500

    Planeación de códigos para listas

---
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.5 Listas.py
@@ -0,0 +1,6 @@
+"""
+-Introducción
+-Segmentación de lista
+-Operadores de lista (No incluir la funciones de pop y append porque irán en la próxima sección)
+-Funciones de lista (Incluir las funciones de compresión de listas)
+"""
