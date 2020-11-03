ef32ac4cdb752d4d1825f4c3a2d74f4573781a46
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-11-03 08:29:05 -0500

    Organización del código y simplificación de los ejemplos de ciclo while.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.5 Listas.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.5 Listas.py
@@ -8,29 +8,27 @@
 # listas en Python, operadores de listas, funciones y métodos de listas
 # los índices empiezan desde cero
 vector = [1,5,6]
-vector2 = ["hola","holados","galleta"]
+vector2 = ["hola","hola_dos","galleta"]
 vector3 = [3.9,2.1,3.0]
 vector4 = "abecedario" #las variables string se pueden manejar como
 #lista de caracteres
 vector_vacío = [] #lista vacío
 vector_múltiple = ["hola",1,1.0,[-1,-2,-3]] #lista con  múltiples tipos de  datos
+#Recorridos con While
 i = 0
 while i < 3:
     print(vector[i])
-    i +=1
-i=0
-while i < 3:
-    print(vector2[i])
-    i +=1
-i=0
-while i < 3:
-    print(vector3[i])
     i +=1
 i=0
 while i < 10:
     print(vector4[i])
     i +=1
 i=0
+# Recorridos con for
+"""
+Ejemplo de recorrido con for
+"""
+
 print(vector_vacío) #puedes imprimir listas completas
 print(vector) # puedes imprimir listas completas
 print(vector_suma) 
@@ -39,6 +37,15 @@ while i < 3:
     print(vector_múltiple[3][i]) #accediendo a listas dentro de listas
     i +=1 #las listas anidados es la forma para trabajar con matrices 
 i=0
+
+# Operaciones entre listas
 vector5 = vector + vector3 #concatenando listas (como si fuera String) por medio de la suma 
 print(vector5)
 print(vector*3) #triplicación de los elementos de la lista (como si fuera String)
+
+"""
+Referencias
+
+-Refernecia notaciones de suma. Quizás en SoloLearn vea más de esto
+
+"""
