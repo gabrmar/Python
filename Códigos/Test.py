19f4d5d11cce111ffc89c4b219a52781c7d25b95
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-11-06 10:31:26 -0500

    Iteraciones en listas usando ciclo for.

--- Códigos/Test.py
+++ Códigos/Test.py
@@ -1,6 +1,16 @@
-import time
 
 
-x = input('valor de x:')
-time.sleep(1)
-print('Valor guardado')
+vector = [1,5,6]
+vector4 = "abecedario" #las variables string se pueden manejar como
+
+
+l1 = len(vector)
+l2 = len(vector4)
+i1 = range(l1)
+i2 = range(l2)
+
+for i in i1:
+    print(vector[i])
+
+for i in i2:
+    print(vector4[i])
--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.5 Listas.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.5 Listas.py
@@ -1,47 +1,64 @@
 """
+Las list
+
 -Introducción
 -Segmentación de lista
 -Operadores de lista (No incluir la funciones de pop y append porque irán en la próxima sección)
 -Funciones de lista (Incluir las funciones de compresión de listas)
 """
 
+"""
+Introducción aquí
+"""
+
 # listas en Python, operadores de listas, funciones y métodos de listas
 # los índices empiezan desde cero
-vector = [1,5,6]
-vector2 = ["hola","hola_dos","galleta"]
-vector3 = [3.9,2.1,3.0]
-vector4 = "abecedario" #las variables string se pueden manejar como
+lista = [1,5,6]
+lista2 = ["hola","hola_dos","galleta"]
+lista3 = [3.9,2.1,3.0]
+lista4 = "abecedario" #las variables string se pueden manejar como
 #lista de caracteres
-vector_vacío = [] #lista vacío
-vector_múltiple = ["hola",1,1.0,[-1,-2,-3]] #lista con  múltiples tipos de  datos
+lista_vacío = [] #lista vacío
+lista_múltiple = ["hola",1,1.0,[-1,-2,-3]] #lista con  múltiples tipos de  datos
 #Recorridos con While
+"""
+Comentarios pendientes
+"""
 i = 0
 while i < 3:
-    print(vector[i])
+    print(lista[i])
     i +=1
 i=0
 while i < 10:
-    print(vector4[i])
+    print(lista4[i])
     i +=1
 i=0
 # Recorridos con for
-"""
-Ejemplo de recorrido con for
-"""
 
-print(vector_vacío) #puedes imprimir listas completas
-print(vector) # puedes imprimir listas completas
-print(vector_suma) 
-print(vector_múltiple)
+"""
+Comentarios
+"""
+l1 = len(lista)
+l2 = len(lista4)
+i1 = range(l1)
+i2 = range(l2)
+for i in i1:
+    print(f"lista, elemento {i}:{lista[i]}")
+for i in i2:
+    print(f"lista4, elemento {i}:{lista4[i]}")
+
+print(lista_vacío) #puedes imprimir listas completas
+print(lista) # puedes imprimir listas completas
+print(lista_múltiple)
 while i < 3:
-    print(vector_múltiple[3][i]) #accediendo a listas dentro de listas
+    print(lista_múltiple[3][i]) #accediendo a listas dentro de listas
     i +=1 #las listas anidados es la forma para trabajar con matrices 
 i=0
 
 # Operaciones entre listas
-vector5 = vector + vector3 #concatenando listas (como si fuera String) por medio de la suma 
-print(vector5)
-print(vector*3) #triplicación de los elementos de la lista (como si fuera String)
+lista5 = lista + lista3 #concatenando listas (como si fuera String) por medio de la suma 
+print(lista5)
+print(lista*3) #triplicación de los elementos de la lista (como si fuera String)
 
 """
 Referencias
