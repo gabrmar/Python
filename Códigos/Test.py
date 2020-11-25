b008fc47474b43b8197760e2c0d7c01f32b2cf40
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-11-24 20:44:29 -0500

    Prueba de potenciaciones sucecivas.

--- Códigos/Test.py
+++ Códigos/Test.py
@@ -1,16 +1,2 @@
-
-
-vector = [1,5,6]
-vector4 = "abecedario" #las variables string se pueden manejar como
-
-
-l1 = len(vector)
-l2 = len(vector4)
-i1 = range(l1)
-i2 = range(l2)
-
-for i in i1:
-    print(vector[i])
-
-for i in i2:
-    print(vector4[i])
+x = 2**3**2
+print(x)
