3b692092b4cfccf8004386611f369021a26ad8d3
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-12-01 21:20:05 -0500

    Pruebas de cada una de las segmentaciones de listas completa.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.6 Listas - Parte 2.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.6 Listas - Parte 2.py
@@ -18,7 +18,9 @@ Esto mismo se usará para hacer segmentación de listas.
 
 # Manejo adicional de listas
 lista = [1,2,3,4,5,6,7,"hola","galleta","comida"]
-# Usa los dos puntos para  solicitar un pedazo (slice) de los elementos de la lista
+
+
+# Usa los dos puntos para solicitar un pedazo (slice) de los elementos de la lista
 p1 = lista[0:1] # Todos son intervalos semiabiertos (el elemento 1 no aprece en p1)
 p2 = lista[1:3] # Todos son intervalos semiabiertos (el elemento 3 no aprece en p2)
 p3 = lista[3:7] # Todos son intervalos semiabiertos (el elemento 7 no aprece en p3)
@@ -26,14 +28,15 @@ p4 = lista[7:9] # Todos son intervalos semiabiertos (el elemento 9 no aprece en 
 p5 = lista[7:] # Desde el elemento 7 hasta el final (elemento 7 sí hace parte de p5)
 p6 = lista[:7] # Desde el inicio hasta el elemento 7, el cual no hará parte de p6
 p7 = lista[::2] # Todos los elementos de la lista con un paso de 2
-p8 = lista[1:9:2] # Todos los elementos entre la posición 1 y 9 con un paso de dos
-# Elemento 7 no hace parte de p8
-p9 = lista[-1:] # Las posiciones negativas se cuentan empezando la lista desde el último elemento
-# "comida" está en la posición -1"
-p10 = lista[-1:2] # Da una lista vacía. No existen elementos más allá de la posición -1
-p11 = lista[-3:-1] # Desde la posición -3 hasta la -1 sin incluir a este último
-p12 = lista[::-1] # Usando los pasos negativos puedes invertir la lista
+p8 = lista[1:9:2] # Todos los elementos entre la posición 1 y 9 con un paso de dos.El elemento 7 ("hola") no hace parte de p8
+p9 = lista[-1:] # Las posiciones negativas se cuentan empezando la lista desde el último elemento. En este caso, "comida" está en la 
+# posición -1"
+leirbp10 = lista[-1:2] # Da una lista vacía. La razón aparente es porque no hay una forma de obtener elementos recorriendo la lista de 
+# izquiera a derecha 
+p11 = lista[-3:-1] # Desde la posición -3 ("hola") hasta la -1 ("comida") sin incluir a este último (recorrido de izquierda a derecha) 
+p12 = lista[::-1] # Por otro lado, sí se pueden usar pasos negativos para hacer un recorrido de derecha a izquierda de una lista.
 
+# -----Imprimiendo segmentos de listas---------------
 
 print(p1)
 print(p2)
