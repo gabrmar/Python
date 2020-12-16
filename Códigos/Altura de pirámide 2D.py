f20b0bf883d8a0febe93eee12f90d6ec4a9558e6
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-12-16 08:37:10 -0500

    Agregando código de altura de pirámide.

---
+++ Códigos/Altura de pirámide 2D.py
@@ -0,0 +1,14 @@
+﻿blocks = int(input("Enter the number of blocks: "))
+
+height=0
+cost=1
+if blocks!=0:
+    while blocks>0:
+        if blocks>=cost:
+            height +=1
+            blocks -=height
+            cost +=1
+        else:
+            break
+
+print("The height of the pyramid:", height)
