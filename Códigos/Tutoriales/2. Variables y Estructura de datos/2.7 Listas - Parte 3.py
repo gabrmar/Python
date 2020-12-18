37966f5d09b9c2c4ed603d53ca9d8e75d30e1dc9
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-12-18 08:51:22 -0500

    Separación de funciones y métodos de listas de las comprensiones de listas en dos códigos de Python.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.7 Listas - Parte 3.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.7 Listas - Parte 3.py
@@ -5,7 +5,7 @@
 4) Verificar si se necesitan referencias para este código
 """
 
-# funciones de listas
+# funciones y métodos de listas
 
 
 #Funciones numéricas para listas
@@ -24,43 +24,4 @@ sumatoria = sum(cubos) #suma de todos los elemento de una lista
 print(sumatoria)
 número = 10
 
-
-#Compresiones de listas
-
-"""
-Las lsitas pueden ser creadas por medio de reglas que definan las cualidades
-de los elementos que los integran. Dichas cualidades son definidas por
-instrucciones de código de manera similar a como una función matemática
-define el trazo de un gráfico
-"""
-
-
-
-#comprensión de lista de cubos
-cubos =[i**3 for i in range(5)] # usa el contador i y lo eleva al cubo
-# en un ciclo desde 0 a 5 (5 no incluido)
-print(cubos)
-
-pares = [i**2 for i in range(10) if i**2%2==0] # puedes agregar condicionales
-# para segiuir especificando las reglas de la lista
-print(pares)
-
-#galleta = [2*i for i in range(10**100)] #habilita esta línea  y tendrás un error de memoria
-# por agotamiento de memoria por usar un rango muy grande para la lista (lista de demsiado tamaño)
-# el programa intentará por un tiemp cumplir con la istrucción, pero fallará y arrojará el MemoryError
-#print(galleta)
-
-
-#..............
-
-
-if all(i > número for i in cubos): #si todos los números son mayores a la variable número
-    print("todos los números son mayores a {n}".format(n = número))
-else:
-    print("No todos los números son mayores a {n}".format(n = número))
-if any(i > 50 for i in cubos): # si cualquiera de los números es mayor a 50
-    print("hay al menos un número mayor a 50")
-else:
-    print("no hay ningún númerio mayor a 50")
-for v in enumerate(cubos): #impirime tuplas de los elementos de la lista con su índice repectivo
-    print(v)
+#Incluir sort, reverse y todas las demás fucniones y métodos de listas encontrados en los marcadores
---
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.8 Listas - Parte 4.py
@@ -0,0 +1,39 @@
+#Compresiones de listas
+
+"""
+Las lsitas pueden ser creadas por medio de reglas que definan las cualidades
+de los elementos que los integran. Dichas cualidades son definidas por
+instrucciones de código de manera similar a como una función matemática
+define el trazo de un gráfico
+"""
+
+
+
+#comprensión de lista de cubos
+cubos =[i**3 for i in range(5)] # usa el contador i y lo eleva al cubo
+# en un ciclo desde 0 a 5 (5 no incluido)
+print(cubos)
+
+pares = [i**2 for i in range(10) if i**2%2==0] # puedes agregar condicionales
+# para segiuir especificando las reglas de la lista
+print(pares)
+
+#galleta = [2*i for i in range(10**100)] #habilita esta línea  y tendrás un error de memoria
+# por agotamiento de memoria por usar un rango muy grande para la lista (lista de demsiado tamaño)
+# el programa intentará por un tiemp cumplir con la istrucción, pero fallará y arrojará el MemoryError
+#print(galleta)
+
+
+#..............
+
+
+if all(i > número for i in cubos): #si todos los números son mayores a la variable número
+    print("todos los números son mayores a {n}".format(n = número))
+else:
+    print("No todos los números son mayores a {n}".format(n = número))
+if any(i > 50 for i in cubos): # si cualquiera de los números es mayor a 50
+    print("hay al menos un número mayor a 50")
+else:
+    print("no hay ningún númerio mayor a 50")
+for v in enumerate(cubos): #impirime tuplas de los elementos de la lista con su índice repectivo
+    print(v)
