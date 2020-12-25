b3e5c66d146282cfa0562e94a8f4859dfce31dad
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-12-24 20:17:07 -0500

    Agregado párrafo de introducción.

--- Códigos/Tutoriales/2. Variables y Estructura de datos/2.7 Listas - Parte 3.py
+++ Códigos/Tutoriales/2. Variables y Estructura de datos/2.7 Listas - Parte 3.py
@@ -1,12 +1,26 @@
 """
-1) Funciones de listas - Incluir las funciones recolectadas en previos códigos
-2) Compresiones de listas - Nuevo código
-3) Verificar si se pueden agregar más fucniones que consideren la separación de esta sección en dos códigos
+
 4) Verificar si se necesitan referencias para este código
 """
+"""
+Las listas tienen una serie de funciones integradas que permiten modificar su contenido sin necesidad de agregar los cambios en una nueva
+variable. Estas funciones se conocen como métodos y estas son algunas de sus características son las siguientes:
 
-# funciones y métodos de listas
-# Colocar definiciones claves que diferencian las funciones de los métodos
+    -Un método tiene la siguente sintaxis: lista.método()
+    -No es necesario guardar los cambios en una neva lista, Es decir que los métodos de lista modifican el contenido de la lista en la cual
+    son llamados.
+
+Por otro lado, también hay funciones que se pueden usar en las listas sin ser necesariamente métodos. Estas funciones cumplen con las siguientes
+características:
+
+    -Una función tiene la siguiente sintaxis: función(argumento_1,argumento_2...argumento_final)
+    -El resultado de las funciones sí puede ser guardado en una nueva variable. Es decir que se puede hacer esto: var = función(argumentos)
+    y luego usar la variable var para el resto del código.
+    -Las funciones no necesariamente modifican el contenido de la lista que se use como argumento a diferencia de los métodos de lista
+    que siempre lo hacen.
+
+En este código podrás ver algunos ejemplos de funciones y métodos de listas en Python
+"""
 
 #Funciones numéricas para listas
 
