de79d6fda9f6d67acaf006d22fd5d7696013a8bf
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-12-23 05:35:49 -0500

    Añadiendo código convertidor de decimal a binario.

---
+++ Códigos/Códigos para Redes de Computadoras/Convertidor Decimal-Binario.py
@@ -0,0 +1,11 @@
+decimal = 200
+binario = [0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ]
+bit = 7
+if decimal > 255:
+    print("El número decimal seleccionado debe ser menor a 256")
+else:
+    for i in binario:
+        binario[bit] = decimal%2
+        decimal = decimal//2
+        bit = bit -1
+    print(binario)
