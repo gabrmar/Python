9f3ff1ed9b26aeb086aa5a663f9f856f05f70555
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-12-26 16:05:29 -0500

    Iniciando pruebas de rutina de conteo de direcciones IPv4.

--- Códigos/Códigos para Redes de Computadoras/Máscaras de Red.py
+++ Códigos/Códigos para Redes de Computadoras/Máscaras de Red.py
@@ -1,4 +1,9 @@
+import ipv4
+
 ip_red = [192, 168 ,10 ,0]
-máscara = 24
-estaciones = pow(2,32-máscara) - 2
-print(estaciones)
+#máscara = 24
+#estaciones = pow(2,32-máscara) - 2
+#print(estaciones)
+
+output = ipv4.count_bin(ip_red,10)
+print(output)
