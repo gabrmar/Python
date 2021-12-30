34fa020da27d5a2e3b47317175bfc3b03e22a560
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-12-29 22:34:15 -0500

    Remoción de fromateo de número binario.

--- Códigos/Códigos para Redes de Computadoras/Convertirdor Binario-Decimal.py
+++ Códigos/Códigos para Redes de Computadoras/Convertirdor Binario-Decimal.py
@@ -1,12 +1,9 @@
 import ipv4
 
-aux = input("Coloca el número binario a convertir: ")
+binario = input("Coloca el número binario a convertir: ")
 
 #-----formateando entrada para que sea una lista de valores enteros 
 
-binario = []
-for i in  aux:
-    binario.append(int(i))
 
 decimal = ipv4.bin2dec(binario) #Está en consideración si se necesita colocar el formateo dentoro de la librería IPv4
 print(decimal) 
