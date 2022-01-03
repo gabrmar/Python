7d4bae2a2be9da301753d540f06e2c71f7c2e5c7
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-01-02 19:02:45 -0500

    Modificando el código para que trabaje con el módulo IPv4.

--- Códigos/Códigos para Redes de Computadoras/Convertidor Decimal-Binario.py
+++ Códigos/Códigos para Redes de Computadoras/Convertidor Decimal-Binario.py
@@ -1,11 +1,6 @@
+import ipv4
+
 decimal = int(input("Coloca el número decimal que quieres convertir a binario: "))
-binario = [0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ]
-bit = 7
-if decimal > 255:
-    print("El número decimal seleccionado debe ser menor a 256")
-else:
-    for i in binario:
-        binario[bit] = decimal%2
-        decimal = decimal//2
-        bit = bit -1
-    print(binario)
+
+binario = ipv4.dec2bin(decimal)
+print(f"El número {decimal} convertido en binario es {binario}")
