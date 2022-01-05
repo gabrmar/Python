c73be331ae0b9ac85421a5d030c5336c214f2235
Gabriel Martínez Llano <Gabriel.Martinez01@csscorp.com>
2022-01-05 10:20:56 -0500

    Remoción de espacios y comentarios innecesarios.

--- Códigos/Códigos para Redes de Computadoras/Convertirdor Binario-Decimal.py
+++ Códigos/Códigos para Redes de Computadoras/Convertirdor Binario-Decimal.py
@@ -1,9 +1,5 @@
 import ipv4
 
 binario = input("Coloca el número binario a convertir: ")
-
-#-----formateando entrada para que sea una lista de valores enteros 
-
-
 decimal = ipv4.bin2dec(binario) #Está en consideración si se necesita colocar el formateo dentoro de la librería IPv4
 print(decimal) 
