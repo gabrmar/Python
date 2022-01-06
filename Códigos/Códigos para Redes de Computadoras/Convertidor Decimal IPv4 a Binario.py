a5ef3317c9d3c2c0fe0da048e21a9a1abe43f3ce
Gabriel Martínez Llano <Gabriel.Martinez01@csscorp.com>
2022-01-06 09:20:34 -0500

    Espacio innecesario removido.

--- Códigos/Códigos para Redes de Computadoras/Convertidor Decimal IPv4 a Binario.py
+++ Códigos/Códigos para Redes de Computadoras/Convertidor Decimal IPv4 a Binario.py
@@ -1,7 +1,6 @@
 import ipv4
 
 dec = ipv4.rand_ipv4()
-
 print(f"Dirección IPv4 en decimal:{dec}")    
 bin_ipv4 = ipv4.dec2bin_ipv4(dec) # Conversión a binario
 print(f"Dirección IPv4 en binario:{bin_ipv4}")
