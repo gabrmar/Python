e795d9587bee57aaca2946826ffb0a6315771913
Gabriel Martínez Llano <Gabriel.Martinez01@csscorp.com>
2022-01-06 09:23:17 -0500

    Implementando rutina generadora de direcciones IPv4 en binario.

--- Códigos/Códigos para Redes de Computadoras/Convertidor Binario IPv4 a Decimal.py
+++ Códigos/Códigos para Redes de Computadoras/Convertidor Binario IPv4 a Decimal.py
@@ -1,13 +1,6 @@
-import ipv4, random
-bin = [[],[],[],[]]  #Plantilla para rellenar una dirección IPv4 aleatoria
-i = 0
-while i < 4:
-    j = 0
-    while j < 8:
-        bin[i].append(random.randint(0,1))
-        j = j + 1
-    i = i + 1
+import ipv4
 
+bin = ipv4.rand_ipv4("b")
 print(f"Dirección IPv4 en binario:{bin}")    
 dec_ipv4 = ipv4.bin2dec_ipv4(bin) # Conversión a decimal 
 print(f"Dirección IPv4 en decimal:{dec_ipv4}")
