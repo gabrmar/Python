0691826893675d84c1f22d7271d0b1924ad4f96f
Gabriel Martínez Llano <Gabriel.Martinez01@csscorp.com>
2022-01-10 10:26:13 -0500

    Definiendo sección donde serán las pruebas pra el formateo de las listas binarias a dirección IPv4.

--- Códigos/Códigos para Redes de Computadoras/Convertidor Binario IPv4 a Decimal.py
+++ Códigos/Códigos para Redes de Computadoras/Convertidor Binario IPv4 a Decimal.py
@@ -1,6 +1,7 @@
 import ipv4
 
 bin = ipv4.rand_ipv4("b")
+#Colocar acá la rutina/función donde se formatea la dirección binaria
 print(f"Dirección IPv4 en binario:{bin}")    
 dec_ipv4 = ipv4.bin2dec_ipv4(bin) # Conversión a decimal 
 print(f"Dirección IPv4 en decimal:{dec_ipv4}")
