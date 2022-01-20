6d770c0e9d54b6c601f02721d2d85c7ed51c1df9
Gabriel Martínez Llano <Gabriel.Martinez01@csscorp.com>
2022-01-20 09:58:37 -0500

    Trasladando líneas de código a al archivo llamado  "Cálculo de Estaciones por Red.py".

--- Códigos/Códigos para Redes de Computadoras/Máscaras de Red.py
+++ Códigos/Códigos para Redes de Computadoras/Máscaras de Red.py
@@ -1,10 +1,3 @@
 import ipv4
 
-ip_red = [192, 168 ,10 ,0]
-#máscara = 24
-#estaciones = pow(2,32-máscara) - 2
-#print(estaciones)
 
-output = ipv4.count_bin(ip_red,10)
-print(output)
-print(type(output))
