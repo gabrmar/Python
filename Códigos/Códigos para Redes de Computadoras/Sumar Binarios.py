12c3e106f3786babf2cf16bd52fbac5e4559152d
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-01-21 09:53:07 -0500

    Probando suma de binarios con números generados aleatoriamente.

--- Códigos/Códigos para Redes de Computadoras/Sumar Binarios.py
+++ Códigos/Códigos para Redes de Computadoras/Sumar Binarios.py
@@ -1,6 +1,13 @@
 import ipv4
 
-a = "10"
-b = "10"
-sum = ipv4.sum_bin(a,b)
+numero = ipv4.rand_ipv4("b")
+numero_2 = ipv4.rand_ipv4("b")
+cadena = ipv4.format_bin(numero[0]) #Sólo se va a extraer una  de las 4 listas que confirman la dirección IPv4 en binario
+cadena_2 = ipv4.format_bin(numero_2[0]) #Esto se debe a que la rutina format_bin() recibe de enetrada una lista, no una lista de listas.
+a = cadena
+b = cadena_2
+print("Número: ",a)
+print("Número 2: ",b)
+sum = ipv4.sum_bin(a,b) #Toca analizar el alcance de esta función de suma cuando los valores superan los límites de un byte
 print(f"La suma de los números binarios es: {sum}")
+
