39c4c5b88fe8da8318db262ca7c7807de16a9cc3
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-02-06 18:38:38 -0500

    Agregando listas para los intereses generados y el capital inicial así como la eliminiación de comentarios en desuso.

--- Códigos/Finanzas con Python/finpy.py
+++ Códigos/Finanzas con Python/finpy.py
@@ -5,7 +5,10 @@ def icompuesto(tasa_interes,cuota,monto):
     interesTotales = -1
     capitalTotal = -1
     proporcionIntereses = -1
-     
+
+    capt = []
+    inter = []
+   
 
     
     #Se necesita hacer una evaluación de los valores de cuota vs montos e intereses para que sean viables para el resto del
@@ -20,6 +23,8 @@ def icompuesto(tasa_interes,cuota,monto):
         while capital > 0:
             intereses = capital*(tasa_interes/100)
             #print("Intereses:",round(intereses,0)) #Habilitar sólo para depurar
+            capt.append(capital)
+            inter.append(intereses)
             interesTotales = interesTotales + intereses
             capital = capital + intereses
             #print("Capital por pagar:",round(capital,0)) #Habilitar sólo para depurar
@@ -29,8 +34,7 @@ def icompuesto(tasa_interes,cuota,monto):
         capitalTotal = capitalTotal + interesTotales
         proporcionIntereses = (interesTotales/monto)*100 #Porcentaje de los intereses con respecto al monto del préstamo
 
-    #print(f"Intereses Pagados:{round(interesTotales,0)} pesos")
-    #print(f"Capital Pagado:{round(capitalTotal,0)} pesos")
-    #print(f"Porcentaje de intereses:{round(proporcionIntereses,2)}%")
+        out = {"Intereses Pagados":interesTotales, "Capital Pagado":capitalTotal, "Porcentaje de intereses":proporcionIntereses}
 
-    return {"Intereses Pagados":interesTotales, "Capital Pagado":capitalTotal, "Porcentaje de intereses":proporcionIntereses}
+
+    return out
