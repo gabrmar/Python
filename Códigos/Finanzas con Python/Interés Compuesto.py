21fc711c629270ec0433820d49a54273db09a2c6
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-02-06 07:01:11 -0500

    Añadido comentario sobre variable de salida.

--- Códigos/Finanzas con Python/Interés Compuesto.py
+++ Códigos/Finanzas con Python/Interés Compuesto.py
@@ -4,6 +4,6 @@ tasa = 1.5 #Interés efectivo mensual
 cuota = 1e3 #Cuota mensual
 monto = 1e5 #Monto adeudado
 
-analisis = finpy.icompuesto(tasa,cuota,monto)
+analisis = finpy.icompuesto(tasa,cuota,monto) #La variable deentrada es un diccionario
 for k,v in analisis.items():
     print(f"{k}: {round(v,0)}")
