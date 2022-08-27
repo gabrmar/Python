50f74eab7884d84c5194b9a8a988bbe76475d6ae
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-08-27 16:24:27 -0500

    Corrigiendo ubicación de archivos de finanzas con Python y agregando archivo de Python para simulador de tarjeta de crédito.

---
+++ Códigos/Finanzas con Python (En Dessarollo)/Interés Compuesto.py
@@ -0,0 +1,20 @@
+import string
+import finpy
+
+tasa = 1.5 #Interés efectivo mensual
+cuota = 7500 #Cuota mensual
+monto = 5e5 #Monto adeudado
+analisis = finpy.icompuesto(tasa,cuota,monto) # La vairbale de salida es un objeto de tipo análisis si el préstamo se puede pagar
+
+print("Evolución del capital:",analisis.lista_capi)
+print("Evolución de los intereses generados",analisis.lista_inter)
+print("Capital pagado incluyendo intereses:",analisis.capitalTotal)
+print("Intereses generados durante el préstamo:",analisis.interesesTotales)
+print(f"Proporción de intereses: {analisis.proporcion}%")
+print(finpy.formato(analisis.interesesTotales))
+#print(finpy.formato(-10)) #Habilita esta función para probar el error de aserción
+#print(finpy.formato(0.123456789)) #Habilita esta función para probar el formato con líneas decimales
+
+#-------Exportar los datos en un archivo de texto -----------
+
+finpy.exportar(analisis)
---
+++ Códigos/Finanzas con Python (En Dessarollo)/Simulador Tarjeta de Crédito.py
@@ -0,0 +1,8 @@
+"""El objetivo de este archivo es simular  una tarjeta de crédito de acuerdo a las clases de finanzas de Platzi y comparar esto con el código de interés compyuesto  la librería finpy"""
+
+#*********Referencias*********
+
+# 1) ¿Como calcular el interés en una tarjeta de crédito?. Enlace: https://www.youtube.com/watch?v=UujNYJ279Os&ab_channel=DavidRivera
+# 2) Qué son, qué no son y cómo funcionan las tarjetas de crédito. Enlace: https://platzi.com/clases/2123-manejo-deuda/33309-que-son-que-no-son-y-como-funcionan-las-tarjetas-d/
+
+
