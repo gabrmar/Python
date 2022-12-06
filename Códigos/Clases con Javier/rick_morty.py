c2010396b386875be90ed7dae0d04b6d6f713cc9
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-12-06 15:21:10 -0500

    Resolviendo punto 6.

--- Códigos/Clases con Javier/rick_morty.py
+++ Códigos/Clases con Javier/rick_morty.py
@@ -12,27 +12,35 @@ json.load convierte el archivo json en un diccionario de Python.
 '''
 
 def Formatear_Salida(diccionario,idioma="IN"):
-    lista_llaves = []
+    if idioma == "ESP":
+        lista_llaves = ["identificador","nombre","fecha emisión","episodiojes","personajes","enlace","fecha creación"]
+    else:
+        lista_llaves = ["id","name","air_date","episode","characters","url","created"]
     lista_valores = []
     lista_personajes = []
-    for llave in diccionario.keys():
-        lista_llaves.append(llave)
     for valor in diccionario.values():
         lista_valores.append(valor)
     i = 0
+    j = 0
     while i < len(lista_valores):
-        if lista_llaves[i] == "characters": # <----como esta confirmada esta lista de personajes?
+        if lista_llaves[i] == "characters" or lista_llaves[i] == "personajes": 
             lista_personajes = lista_valores[i]
-        #print(lista_llaves[i]+": "+str(lista_valores[i])+"\n")
-        print(lista_personajes)
+            print(lista_llaves[i]+":")
+            while j < len(lista_personajes):
+                print(str(j+1)+"- "+lista_personajes[j])
+                j=j+1
+            print("\n")
+        else:
+            print(lista_llaves[i]+": "+str(lista_valores[i])+"\n")
         i = i+1
    
 
 
-def Encontrar_Episodio(lista,episodio):
+def Encontrar_Episodio(lista,episodio,idioma="IN"):
     for i in lista:
         if i["id"] == episodio:
-            salida = Formatear_Salida(i)
+            salida = Formatear_Salida(i,idioma)
+            print("\n")
             break 
 
 
@@ -57,9 +65,21 @@ traduccion = {
 archivo =  open('C:/Users/gm_ll/OneDrive - UNIVERSIDAD AUTONOMA DEL CARIBE/Backup/Gmecatronics/Sistemas informáticos/Python/Códigos/Clases con Javier/episodes.json')
 datos = json.load(archivo)
 print('La variable datos es de tipo: ', type(datos))
-print("la longitud de variable datos es ", len(datos))
-#print(datos["info"])
-#print(datos["results"]) <----  es una lista
 resultados = datos["results"]
-Encontrar_Episodio(resultados,19)
 
+#----------Punto 1----------------
+print("-------------Punto 1--------------")
+print("la longitud de variable datos es ", len(datos))
+
+#----------Punto 4----------------
+print("-------------Punto 4--------------")
+
+Encontrar_Episodio(resultados,19)
+#----------Punto 5----------------
+print("-------------Punto 5--------------")
+Encontrar_Episodio(resultados,8,"ESP")
+
+#----------Punto 6----------------
+print("-------------Punto 6--------------")
+for i in range(len(resultados)+1):
+    Encontrar_Episodio(resultados,i,"ESP")
