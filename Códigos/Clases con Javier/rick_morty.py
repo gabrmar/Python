47de95aff890cbba279d3b8afb18781fd0c1d525
Gabriel Martínez Llano <gmll920628@gmail.com>
2022-12-06 13:08:32 -0500

    creando funciones de fomateo de salidas

--- Códigos/Clases con Javier/rick_morty.py
+++ Códigos/Clases con Javier/rick_morty.py
@@ -11,6 +11,32 @@ Tanto JSON como los dict de python tienen estructuras muy similares.
 json.load convierte el archivo json en un diccionario de Python.
 '''
 
+def Formatear_Salida(diccionario,idioma="IN"):
+    lista_llaves = []
+    lista_valores = []
+    lista_personajes = []
+    for llave in diccionario.keys():
+        lista_llaves.append(llave)
+    for valor in diccionario.values():
+        lista_valores.append(valor)
+    i = 0
+    while i < len(lista_valores):
+        if lista_llaves[i] == "characters": # <----como esta confirmada esta lista de personajes?
+            lista_personajes = lista_valores[i]
+        #print(lista_llaves[i]+": "+str(lista_valores[i])+"\n")
+        print(lista_personajes)
+        i = i+1
+   
+
+
+def Encontrar_Episodio(lista,episodio):
+    for i in lista:
+        if i["id"] == episodio:
+            salida = Formatear_Salida(i)
+            break 
+
+
+
 import json
 
 
@@ -35,6 +61,5 @@ print("la longitud de variable datos es ", len(datos))
 #print(datos["info"])
 #print(datos["results"]) <----  es una lista
 resultados = datos["results"]
-for i in resultados:
-    if i["id"] == 19:
-        print(i)    
+Encontrar_Episodio(resultados,19)
+
