d302ac6f8cb2f847548b647fd04968785dc411b6
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-01-16 21:24:01 -0500

    Añadiendo clase grúa y sus métodos.

--- Códigos/Clases con Javier/Tareas/Carro de Carreras/Carro_Main.py
+++ Códigos/Clases con Javier/Tareas/Carro de Carreras/Carro_Main.py
@@ -1,5 +1,6 @@
-from carro import formula_1
+from carro import formula_1,grua
 
+caterpillar = grua(0)
 ferrari = formula_1(0,modelo=2009,motor="gasolina",cilindros=12)
 ferrari.medir_gasolina()
 ferrari.nivel_gasolina = 1
@@ -8,5 +9,10 @@ print(ferrari.peso)
 ferrari.pesar()
 print(ferrari.peso)
 print(dir(ferrari))
+caterpillar.medir_gasolina()
+print(ferrari.peso)
+caterpillar.remolcar_carro(ferrari.peso)
+caterpillar.peso_carga = 100
+caterpillar.remolcar_carro(ferrari.peso)
 
 
--- Códigos/Clases con Javier/Tareas/Carro de Carreras/carro.py
+++ Códigos/Clases con Javier/Tareas/Carro de Carreras/carro.py
@@ -23,3 +23,23 @@ class formula_1:
 
     def pesar(self):
         self.peso = random.sample(pesos,1)[0]
+
+class grua:
+
+    def __init__(self,kilometraje,nivel_gasolina=20,peso=None,peso_carga=20):
+        self.kilometraje = kilometraje
+        self.nivel_gasolina = nivel_gasolina
+        self.peso = peso
+        self.peso_carga = peso_carga
+
+    def medir_gasolina(self):
+        if self.nivel_gasolina < 2:
+            print("Gasolina baja Llenar el tanque lo más pronto posible.")
+        else:
+            print("Gasolina: {}".format(self.nivel_gasolina))
+
+    def remolcar_carro(self,peso_carro):
+        if self.peso_carga >= peso_carro:
+            print("remolcando carro")
+        else:
+            print("el carro pesa {}, demsaido pesado para ser remolcado por esta grúa".format(peso_carro))
