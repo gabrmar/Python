1700e1a8a784278894d2afb5d93b850ff833228b
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-01-16 20:26:28 -0500

    Añadiendo dunder intit y **kwargs a la clase carro.

--- Códigos/Clases con Javier/Tareas/Carro de Carreras/Carro_Main.py
+++ Códigos/Clases con Javier/Tareas/Carro de Carreras/Carro_Main.py
@@ -1,8 +1,12 @@
 from carro import formula_1
 
-ferrari = formula_1()
+ferrari = formula_1(0,modelo=2009,motor="gasolina",cilindros=12)
 ferrari.medir_gasolina()
 ferrari.nivel_gasolina = 1
 ferrari.medir_gasolina()
+print(ferrari.peso)
+ferrari.pesar()
+print(ferrari.peso)
+print(dir(ferrari))
 
 
--- Códigos/Clases con Javier/Tareas/Carro de Carreras/carro.py
+++ Códigos/Clases con Javier/Tareas/Carro de Carreras/carro.py
@@ -1,9 +1,25 @@
+import random
+
+rango = range(80,120)  #Como hacer esto en una sola línea?
+pesos = []
+for i in rango:
+    pesos.append(i)
+
 class formula_1:
-    kilometraje = 0
-    nivel_gasolina = 10
+    
+    def __init__(self,kilometraje,nivel_gasolina=10,peso=None,**kwargs):
+        self.kilometraje = kilometraje
+        self.nivel_gasolina = nivel_gasolina
+        self.peso = peso
+
+        for atributo,valor in kwargs.items():
+            setattr(self,atributo,valor)
 
     def medir_gasolina(self):
         if self.nivel_gasolina < 2:
             print("Gasolina baja Llenar el tanque lo más pronto posible.")
         else:
             print("Gasolina: {}".format(self.nivel_gasolina))
+
+    def pesar(self):
+        self.peso = random.sample(pesos,1)[0]
