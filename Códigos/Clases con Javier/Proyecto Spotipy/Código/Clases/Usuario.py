551028f1ca5cb3151bcb1a2646dc9d67f63234e2
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-02-24 07:34:42 -0500

    Añadiendo clase usuario.

---
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Clases/Usuario.py
@@ -0,0 +1,7 @@
+
+class usuario:
+    def __init__(self,nombre,correo,celular,contraseña):
+        self.nombre = nombre
+        self.correo = correo
+        self.celular = celular
+        self.contraseña = contraseña
