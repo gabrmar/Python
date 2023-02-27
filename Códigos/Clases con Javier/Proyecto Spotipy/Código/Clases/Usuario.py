323240d3b1cbf4f7237f51b9a025ba5f2dfbf1a9
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-02-26 23:27:10 -0500

    Agregando método mágico __str__ y plantilla para la protección de contraseña.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/Clases/Usuario.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Clases/Usuario.py
@@ -5,3 +5,11 @@ class usuario:
         self.correo = correo
         self.celular = celular
         self.contraseña = contraseña
+
+    def proteger_contraseña(self):
+        #self.contraseña será usado para entregar una contraseña protegida
+        pass
+
+    def __str__(self):
+        return"Clase tipo {a}\nNombre:{b}\nCorreo:{c}\nCelular:{d}\nContraseña:{e}".format(a=type(self),b=self.nombre,c=self.correo,d=self.celular,
+                                                                                           e=self.contraseña)
