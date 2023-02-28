833ba2939ae75c1ac405f4a4cde566a04a582861
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-02-27 23:14:50 -0500

    Añadiendo base de datos de canciones.

---
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Bases_de_Datos/Canciones.py
@@ -0,0 +1,9 @@
+
+Nombre = ["Yo soy de otra clase"]
+Género = ["Rap"]
+Autor = ["Peminen"]
+Letra = ["Lo siento, pero yo no heredo de cualquier clase"]
+canciones = {"Nombre":Nombre,"Género":Género,"Autor":Autor,"Letra":Letra}
+
+def acceder_canciones():
+    return canciones
