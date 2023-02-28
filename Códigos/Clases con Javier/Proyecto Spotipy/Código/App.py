6153f5ab3667e6e8f5c1051e7d2506b851c3bd64
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-02-27 23:14:26 -0500

    Añadiando líneas base para la reproducción de música.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/App.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/App.py
@@ -1,6 +1,7 @@
 import time
 from Clases.Usuario import usuario
 from Módulos.ES_mánager import registrar,validar_registro
+from Bases_de_Datos.Canciones import acceder_canciones
 
 print("Bienvenidos a Spotipy")
 time.sleep(2)
@@ -13,3 +14,14 @@ if teclado == "2":
     u = registrar()
     print(u)
     validar_registro()
+"""x = acceder_canciones() #De esta manera es quye voy a buscar canciones.
+print(x) #Sin embargo, también puedo convertir esto en una función de la clase usuario
+"""
+"""
+Objetivo
+-Colocar el menú de búsqueda de cacniones
+-Hacer la rutina de búsqueda
+-Definir si esta será una función o un método de la clase usuario.
+-Colocar la rutina de reprodiucción de música dentro del ES_mánager-.
+
+"""
