30b5693e7df9632b48481683d87f6f3f264ec1b6
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-02-28 18:44:11 -0500

    Quitando comentarios innecesario en código de excepciones.

--- Códigos/Tutoriales/Excepciones (try y except).py
+++ Códigos/Tutoriales/Excepciones (try y except).py
@@ -8,48 +8,48 @@ un bloque de código establecido de los cuales podemos modificar para establecer
 personlizadas. La lista de ecepciones peredefinidas está disponible en internet.
 """
  
-##for i in range(3):  #definiendo ciclo indefinodo
-##    num1 = float(input("Agrega un número: "))
-##    num2 = float(input("Agrega un número: "))
-##    nombre= input("nombre de la operación: ")
-##    try: #bloque de código que se ejecuta nromalmente y donde puede ocurrir un error
-##       print(i+1) 
-##       div = num1/num2 
-##       print (div)
-##       print("Done calculation")
-##       print("operación" + nombre + "da como resultado "  + div) #esta línea genera un error de tipo
-##       # por no haber convertido la variable  div a string
-##    except ZeroDivisionError: # especificación del tipo de excepción (built-in excepcion)
-##       print("error ocurrió\npor división por cero")
-##    except(ValueError,TypeError): #múltiples excepciones con un solo except
-##    #agrupando las excepciones entre paréntesis
-##        print("error por valor o tipo equivocado")
-##    finally: # Bloque de código que se ejecuta en todo momento (haya o no error)
-##        print("instrucción de finally")
-##        
-##for i in range(3):
-##    print("manejando una excepción para todo error")
-##    num1 = float(input("Agrega un número: "))
-##    num2 = float(input("Agrega un número: "))
-##    try:
-##        div = num1/num2
-##        print(div)
-##        print("Done calculation")
-##    except: #excepción para todo tipo de error
-##        print("Ha ocurrido un error. No preguntes cuál es porque este es una excepción general (para todo error)")
-##
-##for i in range(3):
-##        print("manejando excepciones con declaración raise")
-##        num1 = float(input("Agrega un número: "))
-##        num2 = float(input("Agrega un número: "))
-##        if num2 == 0:
-##            #raise ValueError #declaración raise nos permite llamar a las excepciones predefinidas sin necesidad
-##            # de usar un try y un except
-##            raise ValueError("Colocaste un cero en el denominador.") #puedes colocar mensajes  en las excepciones
-##            #Persnalizadas de Ptython. quita el comentario del raise de arriba si no quieres un mensaje propio
-##        div = num1/num2
-##        print(div)
-##        print("Done calculation")
+for i in range(3):  #definiendo ciclo indefinodo
+    num1 = float(input("Agrega un número: "))
+    num2 = float(input("Agrega un número: "))
+    nombre= input("nombre de la operación: ")
+    try: #bloque de código que se ejecuta nromalmente y donde puede ocurrir un error
+        print(i+1) 
+        div = num1/num2 
+        print (div)
+        print("Done calculation")
+        print("operación" + nombre + "da como resultado "  + div) #esta línea genera un error de tipo
+    # por no haber convertido la variable  div a string
+    except ZeroDivisionError: # especificación del tipo de excepción (built-in excepcion)
+        print("error ocurrió\npor división por cero")
+    except(ValueError,TypeError): #múltiples excepciones con un solo except
+    #agrupando las excepciones entre paréntesis
+        print("error por valor o tipo equivocado")
+    finally: # Bloque de código que se ejecuta en todo momento (haya o no error)
+        print("instrucción de finally")
+        
+for i in range(3):
+    print("manejando una excepción para todo error")
+    num1 = float(input("Agrega un número: "))
+    num2 = float(input("Agrega un número: "))
+    try:
+        div = num1/num2
+        print(div)
+        print("Done calculation")
+    except: #excepción para todo tipo de error
+        print("Ha ocurrido un error. No preguntes cuál es porque este es una excepción general (para todo error)")
+
+for i in range(3):
+        print("manejando excepciones con declaración raise")
+        num1 = float(input("Agrega un número: "))
+        num2 = float(input("Agrega un número: "))
+        if num2 == 0:
+            #raise ValueError #declaración raise nos permite llamar a las excepciones predefinidas sin necesidad
+            # de usar un try y un except
+            raise ValueError("Colocaste un cero en el denominador.") #puedes colocar mensajes  en las excepciones
+            #Persnalizadas de Ptython. quita el comentario del raise de arriba si no quieres un mensaje propio
+        div = num1/num2
+        print(div)
+        print("Done calculation")
 
 print("prueba de raise general")
 try:
