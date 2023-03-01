c54e9b155e1e23092beb075d3b8a885996d09ce0
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-02-28 19:33:39 -0500

    Añadiendo código de excepciones.

---
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Módulos/Excepciones.py
@@ -0,0 +1,12 @@
+class except_mánager:
+
+    def revisar_registro(lista_datos):
+        #El objetivo es reivsar los datos presentes en la lista de dato y dar excepciones para los siguoentes casos:
+        #   -Cuando el correo no tiene el símbolo arroba (@).
+        #   -Cuando el número de celular no tiene la longitud propia de un número para celulares.
+        #   -Cuando el númerop de celular tiene caracteres que no son numéricos.
+        """
+        No estoy seguro si podré usar varias veces la misma excepción, pero creo que sí podré hacerlo con el uso
+        del raise.
+        """
+        pass
