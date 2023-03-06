e65c4910896b367a3031af982ddc1075068bd9f7
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-05 19:18:01 -0500

    Añadiendo rutina para consultar usuarios.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/Bases_de_Datos/Base_Usuarios.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Bases_de_Datos/Base_Usuarios.py
@@ -2,3 +2,8 @@ from Clases import Usuario
 
 admin = Usuario("admin","admin@gmail.com","3006983847","admin")
 gabo = Usuario("Gabriel","gmll920628@gmail.com","3006983847","gabo")
+
+lista_usuarios = [admin,gabo]
+
+def consultar_usarios():
+    return lista_usuarios
