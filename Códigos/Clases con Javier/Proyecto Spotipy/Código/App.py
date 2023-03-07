36a4318bb868142f9811e6b33fb9fc02795d17b2
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-07 13:55:04 -0500

    Simplificación de la sección posterior al inicio de sesión o registro, es decir la parte donde el usuario está activo.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/App.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/App.py
@@ -2,6 +2,22 @@ import time
 from Clases.Usuario import usuario
 from Módulos.ES_mánager import registrar,validar_registro,iniciar_sesión
 
+def pos_inicio(): #Tal vez se pueden inclir como parte de la clase ES_Mánager
+    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
+    if teclado == "1":
+        teclado = input("Coloca el nombre de la canción a buscar:")
+        canción = u.buscar_canciones(teclado)
+        print(canción) #Probar esta variante para casos exitosos y fracasos
+        while type(canción) == str:
+            print("Intenta de nuevo o escribe x para salir")
+            time.sleep(2)
+            teclado = input("Coloca el nombre de la canción a buscar:")
+            if teclado == "x" or teclado == "X":
+                break
+            canción = u.buscar_canciones(teclado)
+            print(canción)
+    
+
 print("Bienvenidos a Spotipy")
 time.sleep(2)
 teclado = input("1. Iniciar Sesión.\n2. Registrarse.\n3. Salir\n")
@@ -12,7 +28,8 @@ if teclado == "1":
     #print(u)
     time.sleep(2)
     # Esta parte de abaajo se puede optimizar de otra forma
-    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
+    pos_inicio()
+"""    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
     if teclado == "1":
         teclado = input("Coloca el nombre de la canción a buscar:")
         canción = u.buscar_canciones(teclado)
@@ -24,14 +41,16 @@ if teclado == "1":
             if teclado == "x" or teclado == "X":
                 break
             canción = u.buscar_canciones(teclado)
-            print(canción)
+            print(canción)"""
+            #-----Acá viene la parte del menú donde se pregunta por reproducir, buscar otra canción o añadir a una lista de reproducción
     #----Hay que agregar la parte donnse se vuelven a pedir los datos. Es posible que se pueda hacer una función al respecto
 if teclado == "2":
     u = registrar()
     print(u)
     validar_registro()
     # Esta parte de abaajo se puede optimizar de otra forma 
-    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
+    pos_inicio() #Hay un problema con el ocultar de la contraseña cuando los datos no son correctos (luego de un N)
+"""    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
     if teclado == "1":
         teclado = input("Coloca el nombre de la canción a buscar:")
         canción = u.buscar_canciones(teclado)
@@ -43,7 +62,8 @@ if teclado == "2":
             if teclado == "x" or teclado == "X":
                 break
             canción = u.buscar_canciones(teclado)
-            print(canción)
+            print(canción)"""
+            #-----Acá viene la parte del menú donde se pregunta por reproducir, buscar otra canción o añadir a una lista de reproducción
 if teclado == "3":
     print("Gracias por usar Spotipy")
 
