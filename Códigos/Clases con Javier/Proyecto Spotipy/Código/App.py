8a3a49a423ac5f94efae5fb4c00e769ce446fb40
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-07 07:30:32 -0500

    Agregando sección de código de búsqueda de la canción en el flujo de inicio de sesión. No obstante, esto se puede mejorar por medio de alguna función.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/App.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/App.py
@@ -2,34 +2,50 @@ import time
 from Clases.Usuario import usuario
 from Módulos.ES_mánager import registrar,validar_registro,iniciar_sesión
 
-
 print("Bienvenidos a Spotipy")
 time.sleep(2)
-teclado = input("1. Iniciar Sesión.\n2. Registrarse.\n")
+teclado = input("1. Iniciar Sesión.\n2. Registrarse.\n3. Salir\n")
 if teclado == "1":
     teclado = input("Usuario:")
     teclado2 = input("Contraseña:")
     u = iniciar_sesión(teclado,teclado2)
-    print(u)
+    #print(u)
     time.sleep(2)
+    # Esta parte de abaajo se puede optimizar de otra forma
+    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
+    if teclado == "1":
+        teclado = input("Coloca el nombre de la canción a buscar:")
+        canción = u.buscar_canciones(teclado)
+        print(canción) #Probar esta variante para casos exitosos y fracasos
+        while type(canción) == str:
+            print("Intenta de nuevo o escribe x para salir")
+            time.sleep(2)
+            teclado = input("Coloca el nombre de la canción a buscar:")
+            if teclado == "x" or teclado == "X":
+                break
+            canción = u.buscar_canciones(teclado)
+            print(canción)
     #----Hay que agregar la parte donnse se vuelven a pedir los datos. Es posible que se pueda hacer una función al respecto
 if teclado == "2":
     u = registrar()
     print(u)
     validar_registro()
-teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
-if teclado == "1":
-    teclado = input("Coloca el nombre de la canción a buscar:")
-    canción = u.buscar_canciones(teclado)
-    print(canción) #Probar esta variante para casos exitosos y fracasos
-    while type(canción) == str:
-        print("Intenta de nuevo o escribe x para salir")
-        time.sleep(2)
+    # Esta parte de abaajo se puede optimizar de otra forma 
+    teclado = input("1. Buscar cacnción\n2. Seleccionar lista\n3. Ajustes.\n")
+    if teclado == "1":
         teclado = input("Coloca el nombre de la canción a buscar:")
-        if teclado == "x" or teclado == "X":
-            break
         canción = u.buscar_canciones(teclado)
-        print(canción)
+        print(canción) #Probar esta variante para casos exitosos y fracasos
+        while type(canción) == str:
+            print("Intenta de nuevo o escribe x para salir")
+            time.sleep(2)
+            teclado = input("Coloca el nombre de la canción a buscar:")
+            if teclado == "x" or teclado == "X":
+                break
+            canción = u.buscar_canciones(teclado)
+            print(canción)
+if teclado == "3":
+    print("Gracias por usar Spotipy")
 
 
 """
