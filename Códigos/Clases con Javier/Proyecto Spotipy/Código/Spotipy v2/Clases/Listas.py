985dfb11da464efd8fe209244c2f0729355aad13
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-11 10:52:29 -0500

    Agregando método mágico __len__ para contar la longitud de la colección de listas de reproducción.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/Clases/Listas.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/Clases/Listas.py
@@ -1,10 +1,13 @@
-from Canciones import canción
+from Clases.Canciones import canción
 
 class listas_reproducción:
     def __init__(self):
-        self.listas = []
+        self.listas = {}
 
     def __str__(self):
         for lista in self.listas:
             #Acá hay que imprimir cada una de las listas de reproducción
             pass 
+
+    def __len__(self):
+        return len(self.listas)
