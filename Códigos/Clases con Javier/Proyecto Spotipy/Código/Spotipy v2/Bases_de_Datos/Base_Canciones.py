9b6b0c668ffaa46cc586b53a5896cfbf06d14f71
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-11 12:45:04 -0500

    Corriendo letra de la cacnión de Peminen.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/Bases_de_Datos/Base_Canciones.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/Bases_de_Datos/Base_Canciones.py
@@ -1,6 +1,6 @@
 from Clases.Canciones import canción
 
-c1 = canción("Yo soy de otra clase","Rap","Peminen","Lo siento, pero to no heredo de cualquier clase")
+c1 = canción("Yo soy de otra clase","Rap","Peminen","Lo siento, pero yo no heredo de cualquier clase")
 c2 = canción("POO","Pop","JD Sensei","POO es otra onda.")
 c3 = canción("El polimorfo","Ranchera","Alejandro el Magic Dunder","No confíes en él, cambia de forma según convenga")
 canciones = [c1,c2,c3]
