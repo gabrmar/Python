a8a643c076abf018796895c5ccc925db1b7b72f2
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-12 23:50:08 -0500

    Transformando módulo ES_mánager en clase IO_mánager y haciendo los ajustes para llamar sus métodos al código prinicipal.

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/App.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/App.py
@@ -1,8 +1,10 @@
 import time
 from Clases.Usuario import usuario
-from Módulos.ES_mánager import registrar,validar_registro,iniciar_sesión,pos_inicio,canción_encontrada
+#from Módulos.ES_mánager import registrar,validar_registro,iniciar_sesión,pos_inicio,canción_encontrada
+from Módulos.ES_mánager import IO_mánager
 
 #Removiendo pos_inicio del main y llevándolo a  ES_mánager
+interfaz = IO_mánager()
 
 print("Bienvenidos a Spotipy")
 time.sleep(2)
@@ -10,22 +12,22 @@ teclado = input("1. Iniciar Sesión.\n2. Registrarse.\n3. Salir\n")
 if teclado == "1":
     teclado = input("Usuario:")
     teclado2 = input("Contraseña:")
-    user  = iniciar_sesión(teclado,teclado2)
+    user  = interfaz.iniciar_sesión(teclado,teclado2)
     #print(user)
     time.sleep(2)
     # Esta parte de abaajo se puede optimizar de otra forma
-    opción = pos_inicio(user)
+    opción = interfaz.pos_inicio(user)
     #-----Acá viene la parte del menú donde se pregunta por reproducir, buscar otra canción o añadir a una lista de reproducción
     #----Hay que agregar la parte donnse se vuelven a pedir los datos. Es posible que se pueda hacer una función al respecto
-    canción_encontrada(opción,user) #---------Revisar esto--------------
+    interfaz.canción_encontrada(opción,user) #---------Revisar esto--------------
 if teclado == "2":
-    user = registrar()
+    user = interfaz.registrar()
     print(user)
-    validar_registro()
+    interfaz.validar_registro()
     # Esta parte de abaajo se puede optimizar de otra forma 
-    opción = pos_inicio(user) #Hay un problema con el ocultar de la contraseña cuando los datos no son correctos (luego de un N)
+    opción = interfaz.pos_inicio(user) #Hay un problema con el ocultar de la contraseña cuando los datos no son correctos (luego de un N)
     #-----Acá viene la parte del menú donde se pregunta por reproducir, buscar otra canción o añadir a una lista de reproducción
-    canción_encontrada(opción,user) #---------Revisar esto--------------
+    interfaz.canción_encontrada(opción,user) #---------Revisar esto--------------
 if teclado == "3":
     print("Gracias por usar Spotipy")
 
