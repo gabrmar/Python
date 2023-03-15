0ed08c06b0054da8cff2a2589d59880a2dec944c
Gabriel Martínez Llano <gmll920628@gmail.com>
2023-03-15 06:25:35 -0500

    Iniciando boceto para función __str__ para la clase lista_repro

--- Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/Clases/Lista_repro.py
+++ Códigos/Clases con Javier/Proyecto Spotipy/Código/Spotipy v2/Clases/Lista_repro.py
@@ -4,8 +4,12 @@ class lista_reproducción:
     def __init__(self,nombre):
         self.nombre = nombre
         self.fila = []
+
+    """ def __str__(self):
+        salida = []
+        for canciones in self.fila:
+            salida.append(canciones)"""
         
     def añadir_a_lista(self,canción,nombre_lista):
         self.fila.append(canción)
         print("Canción {nombre_canción} añadida a la lista {lista}".format(nombre_canción=canción.nombre,lista=nombre_lista))
-
